60baa072f5649474529b2ac9c7e35e73dbfd55f8
조현민 <hyeonmean@gmail.com>
2021-06-09 21:45:06 +0900

    fix : BOJ 1934 S5 최소공배수

--- Hyeonmin/src/week15/BOJ_1934_S5_최소공배수/BOJ_1934_S5_최소공배수.java
+++ Hyeonmin/src/week15/BOJ_1934_S5_최소공배수/BOJ_1934_S5_최소공배수.java
@@ -18,19 +18,36 @@ public class BOJ_1934_S5_최소공배수 {
 			num1 = Integer.parseInt(st.nextToken());
 			num2 = Integer.parseInt(st.nextToken());
 			
+			//두 수 중에 더 작은 것을 num1에 저장
+			if(num1 > num2) {
+				int temp = num1;
+				num1 = num2;
+				num2 = temp;
+			}
+			
+			//두 수의 곱 
+			int multi = num1 * num2;
+			
 			//두 수의 최대 공약수
 			int factor = 0;
 			
-			for (int i = num1; i > 0 ; i--) {
-				//최대 공약수를 구하면 반복을 종료
-				if(num2 % i == 0 && num1 % i == 0) {
-					factor = i;
+			//유클레드 호제법
+			while(true) {
+				int num = num2%num1;
+				
+				//최대 공약수를 구했으면 저장하고 종료
+				if(num == 0) {
+					factor = num1;
 					break;
 				}
+				
+				//다음 진행
+				num2 = num1;
+				num1 = num;
 			}
 			
-			//최대 공배수는 두수의 곱에서 최대 공약수를 나눈 값
-			System.out.println(num1 * num2 / factor);
+			//최대 공배수는 두 수의 곱에서 최대 공약수를 나눈 값
+			System.out.println(multi / factor);
 		}
 	}
 }
