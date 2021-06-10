46d77b427e9556be93a9a155134d32e6c76b1b8b
조현민 <hyeonmean@gmail.com>
2021-06-10 21:27:41 +0900

    fix : BOJ 10773 S4 제로

--- Hyeonmin/src/week15/BOJ_10773_S4_제로/BOJ_10773_S4_제로.java
+++ Hyeonmin/src/week15/BOJ_10773_S4_제로/BOJ_10773_S4_제로.java
@@ -12,22 +12,27 @@ public class BOJ_10773_S4_제로 {
 		
 		K = Integer.parseInt(br.readLine());
 		
+		//부른 숫자를 저장할 배열
 		arr = new int[K];
 		
+		//숫자를 저장해야될 배열의 인덱스
 		int idx = 0;
 		
 		for (int i = 0; i < K; i++) {
 			int num = Integer.parseInt(br.readLine());
 			
+			//0을 부르면 마지막 저장 숫자를 0으로 변경
 			if(num == 0) {
 				arr[idx-1] = 0;
 				idx--;
 			}
+			//숫자 저장
 			else {
 				arr[idx++] = num;
 			}
 		}
 		
+		//모든 수를 더함
 		for (int i = 0; i < K; i++) {
 			sum +=arr[i];
 		}
