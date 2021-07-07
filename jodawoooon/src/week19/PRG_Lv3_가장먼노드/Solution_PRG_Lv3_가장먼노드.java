49ec70a93d5150a96c908c9c92d3fca77bc1be79
multicampus <multicampus@host.docker.internal>
2021-07-07 20:47:42 +0900

    code : Programmers Lv3 가장 먼 노드

---
+++ jodawoooon/src/week19/PRG_Lv3_가장먼노드/Solution_PRG_Lv3_가장먼노드.java
@@ -0,0 +1,75 @@
+package week19.PRG_Lv3_가장먼노드;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution_PRG_Lv3_가장먼노드 {
+	static int answer;
+    static int max;
+	public static void main(String[] args) {
+		int n = 6;
+		int[][] vertex = {{3,6}, {4,3}, {3,2}, {1,3}, {1,2}, {2,4}, {5,2}};
+		System.out.println(solution(n, vertex));
+	}
+	
+	
+    public static int solution(int n, int[][] edge) {
+        // 1번 노드에서 가장 멀리 떨어진 노드의 갯수
+    	
+        bfs(n, edge); //최단경로
+        
+        return answer;
+    }
+    
+    private static void bfs(int n, int[][] edge){
+        Queue<Node> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n+1];
+        
+        //1번부터
+        queue.add(new Node(1,1));
+        visited[1] = true;
+        
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            int curr = node.curr;
+            int moveCnt = node.moveCnt;
+            
+           
+            if(moveCnt>max){ //유일하게 가장 멀리 떨어진 노드일경우
+            	max = moveCnt; //이동거리를 max 전역변수에 갱신
+            	answer=1; //갯수는 1로 초기화
+            	
+            }else if(moveCnt==max){ //동일 이동거리의 노드가 여러개일경우
+                answer++; //갯수 증가
+            }
+            
+            for(int[] v : edge){
+            	int n1 = v[0];
+            	int n2 = v[1];
+            	
+            	//연결 노드 찾아서 이동
+            	
+                if(curr==n1) {
+                	if(visited[n2]) continue;
+                	visited[n2]=true;
+                	queue.add(new Node(n2, moveCnt+1));
+                	
+                }else if(curr==n2) {
+                	if(visited[n1]) continue;
+                	visited[n1]=true;
+                	queue.add(new Node(n1, moveCnt+1));
+                }
+                
+            } 
+        }
+    }
+    
+    static class Node{
+        int curr;
+        int moveCnt;
+        public Node(int curr, int moveCnt){
+            this.curr = curr;
+            this.moveCnt = moveCnt;
+        }
+    }
+}
