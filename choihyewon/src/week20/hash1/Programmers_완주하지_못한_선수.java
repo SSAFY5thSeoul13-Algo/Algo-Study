5b20e65bdff258d8bec6d7da9fbf68f2e52c2686
choehyewon <choehyewon@172.30.1.21>
2021-07-18 19:39:51 +0900

    code : PROGRAMMERS LV1 완주하지 못한 선수

--- choihyewon/src/week20/hash1/Programmers_완주하지_못한_선수.java
+++ choihyewon/src/week20/hash1/Programmers_완주하지_못한_선수.java
@@ -1,5 +1,34 @@
 package week20.hash1;
 
+import java.util.HashMap;
+
 public class Programmers_완주하지_못한_선수 {
+	public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> hm = new HashMap<String,Integer>();
+        for(int i=0; i<participant.length; i++) {
+        	String person = participant[i];
+        	
+        	if(!hm.containsKey(person)) {
+        		hm.put(person, 1);
+        	}else {
+        		hm.put(person, hm.get(person)+1);
+        	}
+        }
+        
+       for(int i=0; i<completion.length; i++) {
+        	String person = completion[i];
+        	hm.put(person, hm.get(person)-1);       	
+        }
+        
+        for (String person : hm.keySet()) {
+			if(hm.get(person) != 0) {
+				answer = person;
+			}
+		}
+        
+        return answer;
+    }
 
 }
