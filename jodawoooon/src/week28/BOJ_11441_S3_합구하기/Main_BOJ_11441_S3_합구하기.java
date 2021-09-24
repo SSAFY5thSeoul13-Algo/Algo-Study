cf313e78b80c479b79c148eda6b50d898504bd74
Daun jo <jodawooooon@naver.com>
2021-09-24 21:13:00 +0900

    code : BOJ 11441 S3 합 구하기

---
+++ jodawoooon/src/week28/BOJ_11441_S3_합구하기/Main_BOJ_11441_S3_합구하기.java
@@ -0,0 +1,46 @@
+package week28.BOJ_11441_S3_합구하기;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * 
+ * ✨ Algorithm ✨
+ * @Problem : BOJ 11411 합구하기
+ * @Author : Daun JO
+ * @Date : 2021. 9. 24. 
+ *
+ */
+public class Main_BOJ_11441_S3_합구하기 {
+	
+	static int N, M, arr[];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N+1];
+
+		st = new StringTokenizer(br.readLine());
+		
+		int sum = 0 ;
+		for(int i = 1; i<=N ; i++) {
+			sum += Integer.parseInt(st.nextToken());
+			arr[i] = sum;
+		}
+		
+		M = Integer.parseInt(br.readLine());
+		for(int i = 0 ; i < M ; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			
+			System.out.println(arr[y]-arr[x-1]); //x~y 구간합
+		}
+	}
+
+}
