486b25cbcb7692828b9dbf9f55253f6bbadcbc4d
Daun jo <jodawooooon@naver.com>
2021-10-02 00:13:38 +0900

    fix : BOJ 1590 S1 캠프가는영식

--- jodawoooon/src/week29/BOJ_1590_S1_캠프가는영식/Main_BOJ_1590_S1_캠프가는영식.java
+++ jodawoooon/src/week29/BOJ_1590_S1_캠프가는영식/Main_BOJ_1590_S1_캠프가는영식.java
@@ -1,10 +1,5 @@
-package week29.BOJ_1590_S1_캠프가는영식;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main_BOJ_1590_S1_캠프가는영식 {
 	static int N, T;
@@ -34,28 +29,27 @@ public class Main_BOJ_1590_S1_캠프가는영식 {
 		
 		Collections.sort(busTimes); //시간 순 정렬
 		
-		if(busTimes.get(0) >= T ) {
-			System.out.println(busTimes.get(0) - T);
-			return;
-		} else if(busTimes.get(busTimes.size() - 1) < T) {
-			System.out.println(-1);
-			return;
-		}
-		
-		//이분탐색
-		long left = 0;
-		long right = busTimes.size() -1;
-		long mid = (left+right)/2;
-		while(left<right) {
-			mid = (left+right)/2;
-			if(busTimes.get((int)mid)==T) {
-				System.out.println(0);
-				return;
+		if(busTimes.get(0) >= T ) ans = busTimes.get(0) - T;
+		else if(busTimes.get(busTimes.size() - 1) < T) ans = -1;
+		else {
+			//이분탐색
+			long left = 0;
+			long right = busTimes.size() -1;
+			long mid = (left+right)/2;
+
+			while(left<right) {
+				mid = (left+right)/2;
+				if(busTimes.get((int)mid)==T) {
+					System.out.println(0);
+                    return;
+				}
+				else if(busTimes.get((int)mid)>T) right = mid;
+				else   left = mid+1;
 			}
-			else if(busTimes.get((int)mid)>T) right = mid;
-			else   left = mid+1;
+            
+            ans = busTimes.get((int)right)-T;
 		}
-		System.out.println(busTimes.get((int)right)-T);
+		System.out.println(ans);
 		
 	}
 
